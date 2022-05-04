7228036700b98ea920ecb9fe115eadcc49245932
VerDel Cargal <verdel@cargal.net>
2022-05-04 11:00:20 -0400

    refactoring with new custom views

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -19,8 +19,6 @@ struct ContentView: View {
   @State private var showingScore = false
   @State private var guessResult = ""
   @State private var roundResults = ""
-  @State private var countries = ["France", "Germany", "Estonia", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]  .shuffled()
-  @State private var correctAnswer = Int.random(in: 0 ..< OPTIONS_PER_GUESS)
   @State private var newScore = 0
 
   var body: some View {
@@ -32,7 +30,7 @@ struct ContentView: View {
         Text("Guess the Flag")
           .font(.largeTitle.weight(.bold))
           .foregroundColor(.white)
-        guessBox()
+        GuessBox(answer: answer)
         Spacer()
         Text("Score: \(newScore)")
           .foregroundColor(.blue)
@@ -53,7 +51,40 @@ struct ContentView: View {
     }
   }
 
-  func guessBox() -> some View {
+  func answer(isCorrect: Bool, correctAnswer: String) {
+    if isCorrect {
+      guessResult = "Correct!"
+      numberCorrect += 1
+      newScore += CORRECT_GUESS_DELTA
+    } else {
+      guessResult = "Wrong. That was the flag for \(correctAnswer)"
+      newScore += INCORRECT_GUESS_DELTA
+    }
+    showingScore = true
+  }
+
+  func checkForEndOfRound() {
+    questionsAskedThisRound += 1
+    if questionsAskedThisRound == QUESTIONS_PER_ROUND {
+      roundComplete = true
+      roundResults = "Your end score is \(newScore). You got \(numberCorrect) out of 8 questions correct."
+    }
+  }
+
+  func startNewRound() {
+    newScore = 0
+    questionsAskedThisRound = 0
+    numberCorrect = 0
+  }
+}
+
+struct GuessBox: View {
+  var answer: (Bool, String) -> ()
+
+  @State private var countries = ["France", "Germany", "Estonia", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+  @State private var correctAnswer = Int.random(in: 0 ..< OPTIONS_PER_GUESS)
+
+  var body: some View {
     VStack(spacing: 15) {
       VStack {
         Text("Tap the flag of")
@@ -66,11 +97,10 @@ struct ContentView: View {
       ForEach(0 ..< 3) { number in
         Button {
           flagTapped(number)
-        } label: {
-          Image(countries[number])
-            .renderingMode(.original)
-            .shadow(radius: 5)
         }
+          label: {
+            FlagImage(country: countries[number])
+          }
       }
     }
     .frame(maxWidth: .infinity)
@@ -80,37 +110,16 @@ struct ContentView: View {
   }
 
   func flagTapped(_ number: Int) {
-    if number == correctAnswer {
-      guessResult = "Correct!"
-      numberCorrect += 1
-      newScore += CORRECT_GUESS_DELTA
-    } else {
-      guessResult = "Wrong. That was the flag for \(countries[number])"
-      newScore += INCORRECT_GUESS_DELTA
-    }
-    showingScore = true
+    answer(number == correctAnswer, countries[correctAnswer])
   }
+}
 
-  func checkForEndOfRound() {
-    questionsAskedThisRound += 1
-    if questionsAskedThisRound == QUESTIONS_PER_ROUND {
-      roundComplete = true
-      roundResults = "Your end score is \(newScore). You got \(numberCorrect) out of 8 questions correct."
-    } else {
-      pickNewFlag()
-    }
-  }
-
-  func pickNewFlag() {
-    countries.shuffle()
-    correctAnswer = Int.random(in: 0 ..< OPTIONS_PER_GUESS)
-  }
-
-  func startNewRound() {
-    pickNewFlag()
-    newScore = 0
-    questionsAskedThisRound = 0
-    numberCorrect = 0
+struct FlagImage: View {
+  var country: String
+  var body: some View {
+    Image(country)
+      .renderingMode(.original)
+      .shadow(radius: 5)
   }
 }
 
